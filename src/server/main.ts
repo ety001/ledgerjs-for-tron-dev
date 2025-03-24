c7d581cc4907f19ab62f6b716399ed6e1dc1b223
ety001 <work@akawa.ink>
2025-03-25 05:27:34 +0800

    add demo for creating speculos emulator

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,16 +1,27 @@
-import express from "express";
-import ViteExpress from "vite-express";
+import express from 'express';
+import ViteExpress from 'vite-express';
 
-import SpeculosTransport from "@ledgerhq/hw-transport-node-speculos";
+import SpeculosTransport from '@ledgerhq/hw-transport-node-speculos';
 import Trx from '@ledgerhq/hw-app-trx';
+
+import { setEnv } from "@ledgerhq/live-env";
+
+import {
+  closeAllSpeculosDevices,
+  releaseSpeculosDevice,
+  createSpeculosDevice,
+  getMemorySpeculosDeviceInternal,
+  modelMap,
+  DeviceModelId,
+} from "@ledgerhq/speculos-transport";
 
 const app = express();
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
+app.get('/hello', (_, res) => {
+  res.send('Hello Vite + React + TypeScript!');
 });
 
-app.get("/transport", async (_, res) => {
+app.get('/transport', async (_, res) => {
   const transport = await SpeculosTransport.default.open({apduPort: 40000});
   const app = new Trx.default(transport);
   const path = `44'/195'/${0}'/0/0`;
@@ -21,6 +32,22 @@ app.get("/transport", async (_, res) => {
   res.send(transport);
 });
 
+app.get('/create_emulator', async (_, res) => {
+  setEnv('SPECULOS_USE_WEBSOCKET', true);
+  const device = await createSpeculosDevice({
+    model: DeviceModelId.nanoS,
+    appName: 'Tron',
+    appVersion: '0.7.0',
+    coinapps: '/home/ety001/workspace/app-tron/build',
+    seed: 'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about',
+    firmware: '2.0',
+    overridesAppPath: 'nanos/bin/app.elf',
+  });
+  console.log(device);
+  res.send({'msg': 'success'});
+});
+
+
 ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+  console.log('Server is listening on port 3000...'),
 );
