faf6d6e5d528b956a56fe0bf30088a4b9d00f763
ety001 <ety001@users.noreply.github.com>
2025-11-28 16:41:44 +0800

    fix building errors and rm hw-app-trx folder (#1)
    Co-authored-by: 3for <zouyudi@gmail.com>
    Co-authored-by: 3for <287494524@qq.com>

--- hw-app-trx/lib-es/TIP712/tip712.d.ts
+++
@@ -1,4 +0,0 @@
-import TIP712 from "./tip712.json";
-export { default as hash } from "./tip712-hash.json";
-export default TIP712;
-//# sourceMappingURL=tip712.d.ts.map
--- hw-app-trx/lib/services/types.d.ts
+++
@@ -1,8 +0,0 @@
-export type LoadConfig = {
-    nftExplorerBaseURL?: string | null;
-    pluginBaseURL?: string | null;
-    extraPlugins?: any | null;
-    cryptoassetsBaseURL?: string | null;
-    calServiceURL?: string | null;
-};
-//# sourceMappingURL=types.d.ts.map
